8797de681ad13ec1a8cea1ef7a2a544f4c027d79
AydinTuna <tunaaydin6161@gmail.com>
2022-07-27 03:00:17 +0300

    Create operation is completed

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="content">
+    <h1>Intro to vue 3</h1>
+    <form @submit.prevent="newTodo">
+      <div class="field">
+        <label class="label">Label</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="Ödev yap" ref="input" />
+        </div>
+      </div>
+      <button class="button is-warning">Ekle</button>
+    </form>
+
+    <div v-for="todo in todos" :key="todo.id" class="card my-5 mx-5">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left"></div>
+          <div class="media-content">
+            <p
+              :class="{ done: todo.done }"
+              @click="toggleDone(todo)"
+              class="title is-4 cursor"
+            >
+              {{ todo.content }}
+            </p>
+            <p class="subtitle is-6">Done: {{ todo.done }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      done: false,
+      todos: [],
+    };
+  },
+  mounted() {
+    this.$refs.input.focus();
+  },
+  methods: {
+    newTodo() {
+      this.todos.push({
+        content: this.$refs.input.value,
+        done: this.done,
+        id: Date.now(),
+      });
+      this.$refs.input.value = "";
+      this.$refs.input.focus();
+    },
+    toggleDone(todo) {
+      todo.done = !todo.done;
+    },
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.cursor {
+  cursor: pointer;
+}
+
+.done {
+  text-decoration: line-through;
 }
 </style>
